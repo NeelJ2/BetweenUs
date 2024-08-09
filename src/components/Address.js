8ba53eea98098cd6e9fb78dd29623b4571e91918
Keenan Long <keenanlong16@gmail.com>
2024-08-09 10:46:15 -0400

    Start of address autocomplete

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Autocomplete from 'react-google-autocomplete';
 import '../assests/Address.css'
 
 function Address() {
@@ -6,11 +7,31 @@ function Address() {
         <div className='Address'>
             <div className='Input_container'>
                 <p>Your Address</p>
-                <input></input>
+                <Autocomplete
+                    apiKey={process.env.REACT_APP_GOOGLE}
+                    style={{width: "50%"}}
+                    onPlaceSelected={(place1) => 
+                    {console.log(place1)}
+                    }
+                    options={{
+                        types: ['address'],
+                        componentRestrictions: { country: "us"}
+                    }}
+                />
             </div>
             <div className='Input_container'>
                 <p>Friend's Address</p>
-                <input></input>
+                <Autocomplete
+                    apiKey={process.env.REACT_APP_GOOGLE}
+                    style={{width: "50%"}}
+                    onPlaceSelected={(place2) => 
+                    {console.log(place2)}
+                    }
+                    options={{
+                        types: ['address'],
+                        componentRestrictions: { country: "us"}
+                    }}
+                />
             </div>
         </div>
     );
