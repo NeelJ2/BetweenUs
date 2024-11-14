694a420c421c02ec87e2330add38bf596e7e9e5e
njoshi24 <njoshi24@terpmail.umd.edu>
2024-11-14 15:30:18 -0500

    fixed the loading issue for the api

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,46 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Autocomplete from 'react-google-autocomplete';
 import '../assests/Address.css';
 
-function Address({ setMyAddress, setFriendsAddress}) {
+function Address({ setMyAddress, setFriendsAddress }) {
+    const [apiLoaded, setApiLoaded] = useState(false);
+
+    useEffect(() => {
+        if (process.env.REACT_APP_GOOGLE) {
+            setApiLoaded(true);
+        }
+    }, []);
 
     return (
         <div className='Address'>
             <div className='Input_container'>
-                <p className='Colored_Text'>Your Address</p>
-                <Autocomplete
-                    apiKey={process.env.REACT_APP_GOOGLE}
-                    style={{width: "50%",
-                        borderRadius: "10px",
-                        textAlign: "center"
-                    }}
-                    onPlaceSelected={(place1) => {
-                        setMyAddress(place1); // Save selected address in state
-                    }}
-                    options={{
-                        types: ['address'],
-                        componentRestrictions: { country: "us" }
-                    }}
-                />
+                <p>Your Address</p>
+                {apiLoaded ? (
+                    <Autocomplete
+                        apiKey={process.env.REACT_APP_GOOGLE}
+                        style={{ width: "50%" }}
+                        onPlaceSelected={(place1) => {
+                            setMyAddress(place1); // Save selected address in state
+                        }}
+                        options={{
+                            types: ['address'],
+                            componentRestrictions: { country: "us" },
+                        }}
+                    />
+                ) : (
+                    <p>Loading address input...</p>
+                )}
             </div>
             <div className='Input_container'>
-                <p className='Colored_Text'>Friend's Address</p>
-                <Autocomplete
-                    apiKey={process.env.REACT_APP_GOOGLE}
-                    style={{width: "50%",
-                        borderRadius: "10px",
-                        textAlign: "center"
-                    }}
-                    onPlaceSelected={(place2) => {
-                        setFriendsAddress(place2); // Save selected address in state
-                    }}
-                    options={{
-                        types: ['address'],
-                        componentRestrictions: { country: "us" }
-                    }}
-                />
+                <p>Friend's Address</p>
+                {apiLoaded ? (
+                    <Autocomplete
+                        apiKey={process.env.REACT_APP_GOOGLE}
+                        style={{ width: "50%" }}
+                        onPlaceSelected={(place2) => {
+                            setFriendsAddress(place2); // Save selected address in state
+                        }}
+                        options={{
+                            types: ['address'],
+                            componentRestrictions: { country: "us" },
+                        }}
+                    />
+                ) : (
+                    <p>Loading address input...</p>
+                )}
             </div>
-
         </div>
     );
 }
