fae44a2effd166622907cb5c9b36045845909c96
njoshi24 <njoshi24@terpmail.umd.edu>
2024-11-18 22:19:32 -0500

    fixed map marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,40 +1,60 @@
-import React, { useEffect } from 'react';
-import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
-import '../assests/Map.css';
+import React, { useEffect } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+} from "@vis.gl/react-google-maps";
+import "../assests/Map.css";
 
-function MapBox({ center, setResultsRadius}) {
-    useEffect(() => {
-        console.log("New center Map")
-    }, [center]);
-    
-    const updateResultsRadius = async () => {
-        setResultsRadius(document.getElementById("Search_Radius").value)
-    }
 
-    window.updateResultsRadius = updateResultsRadius
-    return (
-        <div className='Map'>
-            <div className='Map_Header'>
-                <p className='Colored_Text'>Search Radius</p>
-                <select name='Search_Radius'
-                        id='Search_Radius'
-                        onChange={updateResultsRadius}>
-                    <option value="1609">1 mile</option>
-                    <option value="4828">3 miles</option>
-                    <option value="8046">5 miles</option>
-                </select>
-            </div>
-            <div className='Map_Box'>
-                <APIProvider apiKey={process.env.REACT_APP_GOOGLE}>
-                    <Map
-                    defaultCenter={center.location}
-                    center={center.location}
-                    zoom={center.zoom}
-                    ></Map>
-                </APIProvider>
-            </div>
-        </div>
-    );
+function MapBox({ center, setResultsRadius }) {
+    const ID = `${center}_MAP_ID`;
+
+  useEffect(() => {
+    console.log("New center Map");
+  }, [center]);
+
+  const updateResultsRadius = async () => {
+    setResultsRadius(document.getElementById("Search_Radius").value);
+  };
+
+  window.updateResultsRadius = updateResultsRadius;
+  return (
+    <div className="Map">
+      <div className="Map_Header">
+        <p className="Colored_Text">Search Radius</p>
+        <select
+          name="Search_Radius"
+          id="Search_Radius"
+          onChange={updateResultsRadius}
+        >
+          <option value="1609">1 mile</option>
+          <option value="4828">3 miles</option>
+          <option value="8046">5 miles</option>
+        </select>
+      </div>
+      <div className="Map_Box">
+        <APIProvider apiKey={process.env.REACT_APP_GOOGLE}>
+          <Map
+            defaultCenter={center.location}
+            center={center.location}
+            zoom={center.zoom}
+            mapId = {ID}
+          >
+            {/* Marker at the center */}
+            <AdvancedMarker position={center.location}>
+              <Pin
+                background={"#0f9d58"}
+                borderColor={"#006425"}
+                glyphColor={"#60d98f"}
+              />
+            </AdvancedMarker>
+          </Map>
+        </APIProvider>
+      </div>
+    </div>
+  );
 }
 
 export default MapBox;
